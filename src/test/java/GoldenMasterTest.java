0c8f70fb2459bd3c25330096fb5a554ecd420625
Jean D'Amore <jdamore@thoughtworks.com>
2017-01-23 21:26:42 +1000

    Testing against the Golden Master

---
+++ src/test/java/GoldenMasterTest.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+import org.apache.commons.io.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+public class GoldenMasterTest {
+	
+ 	@Test
+ 	public void goldenMasterTest() {
+		int i = 1;
+		for (Customer inputCustomer : GoldenMasterCreate.goldenMasterInputs()) {
+ 			String expectedStatement = readFromGoldenMasterFile("input-"+i+".txt");
+ 			assertThat(inputCustomer.statement(), is(expectedStatement));
+			i++;
+ 		}
+ 	}
+
+ 	/* 
+  * Private methods
+  */
+
+  private String readFromGoldenMasterFile(String fileName) {
+		String content = "";
+  	try {
+  		File directory = new File(".");
+			File file = new File(
+				directory.getCanonicalPath() + File.separator + 
+				"src" + File.separator + 
+				"test" + File.separator + 
+				"java" + File.separator + 
+				"goldenmaster",
+				fileName);
+			if(file.exists()) { 
+				try (FileInputStream inputStream = new FileInputStream(file.getCanonicalPath())) {   
+    			content = IOUtils.toString(inputStream);
+    		}
+			}
+		}
+		catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		return content;
+  }
+
+
+}
