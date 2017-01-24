3c455e34e2f05e123c29beb1b12d5869cb95b75c
Jean D'Amore <jdamore@thoughtworks.com>
2017-01-24 16:38:45 +1000

    Removing temps

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -18,21 +18,25 @@ class Customer {
 	}
 
 	public String statement () { 
-		double totalAmount = 0;
-		int frequentRenterPoints = 0;
 		Enumeration rentals = _rentals.elements();
 		String result = "Rental Record for " + getName() + "\n"; 
 		while (rentals.hasMoreElements()) {
 			Rental each = (Rental) rentals.nextElement();
-			frequentRenterPoints += each.getFrequentRenterPoints();
 			//show figures for this rental
 			result += "\t" + each.getMovie().getTitle()+ "\t" + String.valueOf(each.getCharge()) + "\n";
-			totalAmount += each.getCharge();
 		}
 		//add footer lines result += "Amount
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+		result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+		result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
 		return result;
+	}
+
+	private double getTotalCharge() { 
+		return _rentals.stream().mapToDouble(r -> ((Rental)r).getCharge()).sum();
+	}
+
+	private int getTotalFrequentRenterPoints() { 
+		return _rentals.stream().mapToInt(r -> ((Rental)r).getFrequentRenterPoints()).sum();
 	}
 
 }
